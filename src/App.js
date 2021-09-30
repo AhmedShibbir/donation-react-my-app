c188c6f7b525872c623c7a0b3853bc29d1c75642
Shibbir Ahmed <shibbirahmedops@gmail.com>
2021-09-30 22:02:46 +0600

    data fetched

--- src/App.js
+++ src/App.js
@@ -1,11 +1,21 @@
+import { useEffect, useState } from 'react';
 import './App.css';
 import Header from './components/Header/Header';
+import "./MOCK_DATA.json";
+import Footer from './components/Footer/Footer'
 
 
 function App() {
+  const [Donations,setDonations]=useState([]);
+  useEffect(()=>{
+    fetch("./MOCK_DATA.json")
+    .then(res => res.json())
+    .then(data => setDonations(data))
+  },[])
   return (
     <div className="App">
-     <Header></Header>
+     <Header Donations={Donations}></Header>
+     <Footer></Footer>
      
     </div>
   );
--- src/components/Header/Header.js
+++ src/components/Header/Header.js
@@ -3,16 +3,18 @@ import "./Header.css"
 
 const Header = () => {
     return (
-        <div className="Header-container">
-            <div>
-                <h1 className="Header-greeting">In Search of a better world!</h1>
+        <div className="Header_Main_Container">
+            <div className="Header-container">
+                <div>
+                    <h4 className="Header-greeting">In Search of a better world!</h4>
+                </div>
+                <nav>
+                    <a href="/Home">Home</a>
+                    <a href="/News">News</a>
+                    <a href="/About">About</a>
+                    <a href="/Contact">Contact</a>
+                </nav>
             </div>
-            <nav>
-                <a href="/Home">Home</a>
-                <a href="/News">News</a>
-                <a href="/About">About</a>
-                <a href="/Contact">Contact</a>
-            </nav>
         </div>
     );
 };
